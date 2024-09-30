213ff870ca819511c08b0234683d584a87a977ce
TuanPhan <phanhoangtuan9493@gmail.com>
2024-09-30 09:20:40 +0700

    commit

--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -4,7 +4,7 @@ import { useSelector } from 'react-redux';
 import { useNavigation } from '@react-navigation/native';
 import { RootState } from '../redux/store';
 import MovieList from '../components/MovieList';
-import { HomeScreenNavigationProp, Movie } from '../types';
+import { HomeScreenNavigationProp } from '../types';
 
 const HomeScreen = () => {
   const movies = useSelector((state: RootState) => state.movies.list);
